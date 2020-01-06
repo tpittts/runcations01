258562a4bac674df96d39718e35a6bb4930136bc
alb12-la <alb12@ucla.edu>
2020-01-05 20:32:05 -0800

    my first commit

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("3 April 2020");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
